ccd53b48e8e39274374c5dec2bce68fd487dea62
Vali <valireyimbergenov79@gmail.com>
2025-08-05 00:01:05 +0500

    update user.service

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/course.entity';
 import { CoursesController } from './course.controller';
 import { CoursesService } from './course.service';
+import { Category } from 'src/category/entities/cateogry.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Category])],
   controllers: [CoursesController],
   providers: [CoursesService],
   exports: [CoursesService],
