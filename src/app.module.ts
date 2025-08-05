72fc85caff95dd9a875fdce4bf585c5b5f3f6f27
Vali <valireyimbergenov79@gmail.com>
2025-08-05 18:27:47 +0500

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,30 +14,40 @@ import { PaymentModule } from './payment/payment.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        autoLoadEntities: true,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        const host = configService.get<string>('DB_HOST') || 'localhost';
+        const port = configService.get<number>('DB_PORT') || 5432;
+        const username = configService.get<string>('DB_USERNAME') || 'postgres';
+        const password = configService.get<string>('DB_PASSWORD') || '';
+        const database = configService.get<string>('DB_NAME') || 'test';
+
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+          synchronize: true,
+          autoLoadEntities: true,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+      },
     }),
+
     AuthModule,
     UsersModule,
     RequestsModule,
     CoursesModule,
     PurchasesModule,
     CategoryModule,
-    PaymentModule
+    PaymentModule,
   ],
 })
 export class AppModule {}
