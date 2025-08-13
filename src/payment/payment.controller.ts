2ccca52fdb25fb2791f97a7f4b840d0f96868ef2
Vali <valireyimbergenov79@gmail.com>
2025-08-13 19:34:13 +0500

    update

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -39,17 +39,26 @@ export class PaymentsController {
     this.logger.log(`To'lov boshlash: userId=${userId}`);
     return await this.paymentsService.startPayment(createPaymentDto, userId);
   }
-@All('webhook')
+
+  @All('webhook')
 @HttpCode(200)
 async handleWebhook(@Req() req: Request) {
   try {
     this.logger.debug(`Webhook headers: ${JSON.stringify(req.headers)}`);
     this.logger.debug(`Webhook raw body: ${JSON.stringify(req.body)}`);
-    this.logger.debug(`Webhook body type: ${typeof req.body}`);
 
-    const rawBody = req.body; // text/plain bo‘lib keladi
-    if (!rawBody || typeof rawBody !== 'string') {
-      this.logger.warn('Webhook bo‘sh keldi yoki string emas');
+    let rawBody: string;
+
+    // Agar body string bo‘lsa — to‘g‘ridan-to‘g‘ri olamiz
+    if (typeof req.body === 'string') {
+      rawBody = req.body;
+    } 
+    // Agar body JSON bo‘lsa — stringga aylantiramiz
+    else if (typeof req.body === 'object' && req.body !== null) {
+      rawBody = JSON.stringify(req.body);
+    } 
+    else {
+      this.logger.warn('Webhook bo‘sh keldi yoki noto‘g‘ri format');
       return { ok: true };
     }
 
@@ -60,5 +69,6 @@ async handleWebhook(@Req() req: Request) {
     return { ok: true };
   }
 }
+
 
 }
